de2e4b89dff696684cdd6ab445470c76c102ac4d
Graham Reeves <graham@grahamreeves.com>
2025-05-08 15:23:07 +0100

    Initial Commit

---
+++ PopCatPrinter/PopCatPrinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopCatPrinterApp.swift
+//  PopCatPrinter
+//
+//  Created by Graham Reeves on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PopCatPrinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
